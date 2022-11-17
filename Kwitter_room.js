f7aa115d8813a4d3cfede4068aa0be3214f984ef
web444 <62865753+web444@users.noreply.github.com>
2022-11-17 16:06:08 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDa_AiZyw2xzm55bRKDL99b33RJxYf86ts",
+  
+    authDomain: "kwitter-project-897d4.firebaseapp.com",
+  
+    databaseURL: "https://kwitter-project-897d4-default-rtdb.firebaseio.com",
+  
+    projectId: "kwitter-project-897d4",
+  
+    storageBucket: "kwitter-project-897d4.appspot.com",
+  
+    messagingSenderId: "848532448862",
+  
+    appId: "1:848532448862:web:860e2c93f47c6986c736cf",
+  
+    measurementId: "G-YG2ZP8Z0PD"
+  
+  };
+  
